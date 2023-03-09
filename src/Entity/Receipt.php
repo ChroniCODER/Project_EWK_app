0e0e13f41edb0dcc5f9f79697879d79d93279d70
ChroniCODER <thomas.leherisse@gmail.com>
2023-03-09 15:56:58 +0100

    modif css bootstrap des bouton filter NAME ET DATE

--- src/Entity/Receipt.php
+++ src/Entity/Receipt.php
@@ -6,8 +6,11 @@ use App\Repository\ReceiptRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ReceiptRepository::class)]
+#[Vich\Uploadable]
 class Receipt
 {
     use TimestampableEntity;
@@ -23,6 +26,10 @@ class Receipt
     #[ORM\ManyToOne(inversedBy: 'receipts')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Product $product = null;
+
+    // NOTE: This is not a mapped field of entity metadata, just a simple property.
+    #[Vich\UploadableField(mapping: 'receipts', fileNameProperty: 'image')]
+    private ?File $imageFile = null;
 
     
     public function getId(): ?int
@@ -56,6 +63,33 @@ class Receipt
 
     public function __toString()
     {
-        return (string) $this->id;    }
+        return (string) $this->id;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
 
 }
